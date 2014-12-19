cd110eee2d77c49a2607ef71db2065d41c692a44
Michelle Guo <shellguo95@gmail.com>
2014-12-18 22:40:56 -0800

    created profile page, containing id, email, and password information

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
 module.exports = {
 
     'url' : 'mongodb://localhost/users'
-    //'mongodb://shellguo95:orangelily95asdf@ds061620.mongolab.com:61620/michelle' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
