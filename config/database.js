40ec7a843a52b703dd3970c3f76e1c1e15e3383e
Michelle Guo <shellguo95@gmail.com>
2014-12-18 21:17:10 -0800

    worked on views

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://localhost/users'
+    //'mongodb://shellguo95:orangelily95asdf@ds061620.mongolab.com:61620/michelle' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
