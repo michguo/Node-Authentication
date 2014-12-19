77a354b465112b4a4d93899b194d449ea43e42df
Michelle Guo <shellguo95@gmail.com>
2014-12-18 22:10:17 -0800

    handled the signup/registration

--- app/routes.js
+++ app/routes.js
@@ -30,6 +30,13 @@ module.exports = function(app, passport) {
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
+    
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
     // process the signup form
     // app.post('/signup', do all our passport stuff here);
@@ -44,6 +51,8 @@ module.exports = function(app, passport) {
             user : req.user // get the user out of session and pass to template
         });
     });
+    
+    
 
     // =====================================
     // LOGOUT ==============================
